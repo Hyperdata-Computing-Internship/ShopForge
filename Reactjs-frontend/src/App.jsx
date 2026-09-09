02ba16fd9bc515161ff276bf651fafdf26537cb0
Syed Zaigham Hussain Shah <zaigham.shah2004@gmail.com>
2026-09-09 18:12:03 +0500

    animated product card component created

--- Reactjs-frontend/src/App.jsx
+++ Reactjs-frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Login from "./components/loginpage/login"
-import Productcards from './components/productspage/productcards'
+import Productcards from './components/productspage/Productcards'
 
 const App = () => {
   return (
       // <Login/>
-      <Productcards/>
+      <Productcards title="Leather Shoes" description="premium shoes" image="https://static.vecteezy.com/system/resources/thumbnails/048/720/410/small_2x/men-s-leisure-leather-shoe-isolated-on-transparent-background-free-png.png"  />
 
   )
 }
--- Reactjs-frontend/src/components/productspage/Productcards.jsx
+++ Reactjs-frontend/src/components/productspage/Productcards.jsx
@@ -1,11 +1,71 @@
 import React from 'react'
+import { useEffect, useRef } from "react";
 
-const Productcards = () => {
+const Productcards = (prop) => {
+  const containerRef = useRef(null);
+  const cardRef = useRef(null);
+  const titleRef = useRef(null);
+
+  useEffect(() => {
+  const container = containerRef.current;
+  const card = cardRef.current;
+  const title = titleRef.current;
+
+  const handleMouseMove = (e) => {
+    const rect = container.getBoundingClientRect();
+    const centerX = rect.left + rect.width / 2;
+    const centerY = rect.top + rect.height / 2;
+
+    const x = (centerX - e.clientX) / 15;
+    const y = (centerY - e.clientY) / 15;
+
+    card.style.transform = `rotateY(${x}deg) rotateX(${y}deg)`;
+  };
+
+  const handleMouseEnter = () => {
+    card.style.transition = "none";
+    title.style.transform = "translateZ(80px)";
+  };
+
+  const handleMouseLeave = () => {
+    card.style.transition = "all 0.5s ease";
+    card.style.transform = "rotateY(0deg) rotateX(0deg)";
+    title.style.transform = "translateZ(0px)";
+  };
+
+  container.addEventListener("mousemove", handleMouseMove);
+  container.addEventListener("mouseenter", handleMouseEnter);
+  container.addEventListener("mouseleave", handleMouseLeave);
+
+  return () => {
+    container.removeEventListener("mousemove", handleMouseMove);
+    container.removeEventListener("mouseenter", handleMouseEnter);
+    container.removeEventListener("mouseleave", handleMouseLeave);
+  };
+  }, []);
+
   return (
-    <div>
-        
-    </div>
-  )
+      <div className="container" ref={containerRef}>
+        <div className="card" ref={cardRef}>
+          <h1 className="title" ref={titleRef}>
+            {prop.title}
+          </h1>
+
+          <div className="shoes">
+            <img src={prop.image} />
+          </div>
+
+          <div className="info">
+            <h3>{prop.description}</h3>
+          </div>
+
+          <div className="purchase">
+            <button>Purchase</button>
+            <button>Add to cart</button>
+          </div>
+        </div>
+      </div>
+  );
 }
 
 export default Productcards
